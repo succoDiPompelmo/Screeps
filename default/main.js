83d7c76fa407ec01f096b75b53770d241f0a38c5
Giovanni Zenari <zenari12@gmail.com>
2020-02-19 21:52:18 +0100

    Added gitignore file and ESlint dependencies

--- default/main.js
+++ default/main.js
@@ -1,33 +1,35 @@
-var RoleHarvester = require('role.harvester');
-var RoleUpgrader = require('role.upgrader');
-var RoleBuilder = require('role.builder')
-var RoleSpawn = require('role.spawn');
+const RoleHarvester = require('role.harvester');
+const RoleUpgrader = require('role.upgrader');
+const RoleBuilder = require('role.builder');
+const RoleSpawn = require('role.spawn');
 
-var upgrader_count = 0;
-var harvester_count = 0;
+let upgraderCount = 0;
+let harvesterCount = 0;
 
-for (var name in Game.creeps){
-    var creep = Game.creeps[name];
-    if (creep.memory.role == "harvester"){
-       RoleHarvester.run(creep);
-       harvester_count++;
+for (const name in Game.creeps) {
+  if (Object.prototype.hasOwnProperty.call(Game.creeps, name)) {
+    const creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester') {
+      RoleHarvester.run(creep);
+      harvesterCount++;
     }
-    if (creep.memory.role == "upgrader"){
-        RoleUpgrader.run(creep);
-        upgrader_count++;
+    if (creep.memory.role == 'upgrader') {
+      RoleUpgrader.run(creep);
+      upgraderCount++;
     }
-    if (creep.memory.role == "builder"){
-        RoleBuilder.run(creep);
-        builder_count++;
+    if (creep.memory.role == 'builder') {
+      RoleBuilder.run(creep);
+      builderCount++;
     }
+  }
 }
 
-if (upgrader_count < 7){
-        RoleSpawn.run("U");
-    }
-if (harvester_count < 2){
-        RoleSpawn.run("H");
-    }
-if (builder_count < 5){
-        RoleBuilder.run("B")
+if (upgraderCount < 7) {
+  RoleSpawn.run('U');
+}
+if (harvesterCount < 2) {
+  RoleSpawn.run('H');
+}
+if (builderCount < 5) {
+  RoleBuilder.run('B');
 }
