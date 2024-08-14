0b7d81c5d5ff93a164f27f0891383421120e34b3
succoDiPompelmo <zenari12@gmail.com>
2024-08-14 18:24:53 +0200

    Rollup set up

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const screeps = require('rollup-plugin-screeps');
+
+let cfg;
+const dest = process.env.DEST;
+if (!dest) {
+  console.log("No destination specified - code will be compiled but not uploaded");
+} else if ((cfg = require("./screeps.json")[dest]) == null) {
+  throw new Error("Invalid upload destination");
+}
+
+module.exports = {
+  input: "src/main.ts",
+  output: {
+    file: "dist/main.js",
+    format: "cjs",
+    sourcemap: true
+  },
+
+  plugins: [
+    resolve({ rootDir: "src" }),
+    commonjs(),
+    typescript({tsconfig: "./tsconfig.json"}),
+    screeps({config: cfg, dryRun: cfg == null})
+  ]
+}
