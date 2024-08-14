f21dd181aecc2b8e2eafb8353dc9c525a49f5019
succoDiPompelmo <zenari12@gmail.com>
2024-08-14 22:54:40 +0200

    Harvesting basics

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ declare global {
     Example types, expand on these or remove them and add your own.
     Note: Values, properties defined here do no fully *exist* by this type definiton alone.
           You must also give them an implemention if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
-
+ 
     Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
     Interfaces matching on name from @types/screeps will be merged. This is how you can extend the 'built-in' interfaces from @types/screeps.
   */
@@ -27,8 +27,20 @@ declare global {
   }
 }
 
+const SPAWN = 'Spawn';
+
 export const loop = () => {
-  console.log(`Current game tick is ${Game.time}`);
+  let total_creeps = Object.keys(Game.creeps).length;
+
+  if (total_creeps < 1) {
+    Game.spawns[SPAWN].spawnCreep([WORK, CARRY, MOVE], 'Harvester', { memory: { role: 'harvester', room: 'W1N1', working: false, works_at: null } });
+  }
+
+  for (const creep_name in Game.creeps) {
+    const creep = Game.creeps[creep_name];
+
+    harvest(creep);
+  }
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
@@ -37,3 +49,18 @@ export const loop = () => {
     }
   }
 };
+
+function harvest(creep: Creep) {
+  if (creep.store.energy < creep.store.getCapacity()) {
+    const sources = creep.room.find(FIND_SOURCES);
+    const closest_source = creep.pos.findClosestByPath(sources);
+
+    if (closest_source && creep.harvest(closest_source) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(closest_source, { visualizePathStyle: { stroke: '#ffaa00' } });
+  } else {
+    if (creep.transfer(Game.spawns[SPAWN], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(Game.spawns[SPAWN], { visualizePathStyle: { stroke: '#ffffff' } });
+    }
+  }
+}
+}
